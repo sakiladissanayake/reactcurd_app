5c04f661db394418e53b17bad1472c33c28938d3
sakiladissanayake <sakilad@gmail.com>
2019-01-03 12:06:21 +0530

    submit the form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/create.cpmponent';
+import Edit from './components/edit.component';
+import Index from './components/index.component'
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>React CURD Tutorial</h2>
-      </div>
+     <Router>
+       <div className="container">
+       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to={'/'} className="navbar-brand">React CURD Example</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={'/'} className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/create'} className="nav-link">Create</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={'/index'} className="nav-link">Index</Link>
+            </li>
+          </ul>
+        </div>
+       </nav><br/>
+       <h2>Welcome to React CURD Tutorial</h2>
+       <Switch>
+         <Route exact path='/create' component={ Create } />
+         <Route path='/edit/:id' component={ Edit } />
+         <Route path='/index' component={ Index } />
+       </Switch>
+       </div>
+     </Router>
     );
   }
 }
